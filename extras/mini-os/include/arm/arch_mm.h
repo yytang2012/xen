eab966ab3a1b2f2cf03e789dd9f47877b5a1a968
Karim Raslan <karim.allah.ahmed@gmail.com>
2014-01-04 23:41:30 +0000

    mini-os: headers for ARM
    Adds header files for future ARM support.
    Based on work by Karim Allah Ahmed.
    
    Signed-off-by: Karim Allah Ahmed <karim.allah.ahmed@gmail.com>
    Signed-off-by: Thomas Leonard <talex5@gmail.com>
    
    ---
    
    Since v5:
    
    Define BUG to cause an undefined instruction fault. This will let
    us dump the registers.
    
    Renamed stack to _boot_stack for clarity.
    
    Include kernel.h from arm/os.h. This is for consistency with x86/os.h
    and is needed to compile with lwIP.
    
    Added paddr_t type to represent physical addresses (requested by Julien
    Grall).

---
+++ extras/mini-os/include/arm/arch_mm.h
@@ -0,0 +1,38 @@
+#ifndef _ARCH_MM_H_
+#define _ARCH_MM_H_
+
+typedef unsigned long paddr_t;
+
+extern char _text, _etext, _erodata, _edata, _end, __bss_start;
+extern int _boot_stack[];
+extern int _boot_stack_end[];
+extern int physical_address_offset;	/* Add this to a virtual address to get the physical address (wraps) */
+
+#define PAGE_SHIFT        12
+#define PAGE_SIZE        (1 << PAGE_SHIFT)
+#define PAGE_MASK       (~(PAGE_SIZE-1))
+
+#define L1_PAGETABLE_SHIFT      12
+
+#define to_phys(x)                 ((paddr_t)(x)+physical_address_offset)
+#define to_virt(x)                 ((void *)((paddr_t)(x)-physical_address_offset))
+
+#define PFN_UP(x)    (((x) + PAGE_SIZE-1) >> L1_PAGETABLE_SHIFT)
+#define PFN_DOWN(x)    ((x) >> L1_PAGETABLE_SHIFT)
+#define PFN_PHYS(x)    ((uint64_t)(x) << L1_PAGETABLE_SHIFT)
+#define PHYS_PFN(x)    ((x) >> L1_PAGETABLE_SHIFT)
+
+#define virt_to_pfn(_virt)         (PFN_DOWN(to_phys(_virt)))
+#define virt_to_mfn(_virt)         (PFN_DOWN(to_phys(_virt)))
+#define mfn_to_virt(_mfn)          (to_virt(PFN_PHYS(_mfn)))
+#define pfn_to_virt(_pfn)          (to_virt(PFN_PHYS(_pfn)))
+
+#define mfn_to_pfn(x) (x)
+#define pfn_to_mfn(x) (x)
+
+#define virtual_to_mfn(_virt)	   virt_to_mfn(_virt)
+
+// FIXME
+#define map_frames(f, n) (NULL)
+
+#endif
