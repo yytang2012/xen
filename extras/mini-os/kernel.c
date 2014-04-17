c277be4caa327f57df28744917c1a77bbfaeddaa
Thomas Leonard <talex5@gmail.com>
2014-04-17 10:41:52 +0100

    Enable regular console
    For some reason, this breaks xenbus test.

--- extras/mini-os/kernel.c
+++ extras/mini-os/kernel.c
@@ -134,10 +134,8 @@ void start_kernel(void)
     /* Init time and timers. */
     init_time();
 
-#ifndef __arm__
     /* Init the console driver. */
     init_console();
-#endif
 
     /* Init grant tables */
     init_gnttab();
